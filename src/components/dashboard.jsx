0b1ae5857dd9d9e00079c3d0b0ce69c4ee47532c
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-29 10:56:51 +0530

    feat: Google keep icon to header

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import '../scss/_dashboard.scss'
 import { Grid, Snackbar, AppBar, IconButton, Toolbar, Typography } from "@material-ui/core";
 import MenuIcon from '@material-ui/icons/Menu';
+import keepIcon from '../assets/googleKeep.png';
 
 
 class Dashboard extends Component {
@@ -10,6 +11,7 @@ class Dashboard extends Component {
 			<Grid item md={12} container >
             <AppBar className="app-bar">
                 <Toolbar className="header">
+					<img src={keepIcon} alt="logo" className="keep-icon" />
                     <Typography variant="h6" className="header-title" >
                        Keep
                     </Typography>
