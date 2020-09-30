1e14cf81dc100c593ce8c609125396c5fccb2a01
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-30 14:14:37 +0530

    feat: Added testcases for login page

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -1,20 +1,52 @@
 import React from 'react';
-import Enzyme, { render, shallow } from 'enzyme';
+import Enzyme, { shallow } from 'enzyme';
+import { render } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-16';
 import LoginPage from '../components/login';
-
-
+import { BrowserRouter as Router } from 'react-router-dom';
 
 React.useLayoutEffect = React.useEffect;
 Enzyme.configure({
-    adapter: new Adapter()
+	adapter: new Adapter(),
 });
 
-describe('Sign In', () => {
-    it('Should be true', () => {
-        const wrapper = render(<LoginPage/>);
-        const fields = wrapper.find('root');
-        console.log(fields);
-        expect(fields).length.toEqual(1);
-    });
+describe('Login page components', () => {
+	it('get text of sign in', () => {
+		const { getByText } = render(
+			<Router>
+				<LoginPage />
+			</Router>
+		);
+		const linkElement = getByText('Sign in');
+		expect(linkElement).toBeInTheDocument();
+	});
+
+	it('get text of continue to Gmail', () => {
+		const { getByText } = render(
+			<Router>
+				<LoginPage />
+			</Router>
+		);
+		const linkElement = getByText('to continue to Gmail');
+		expect(linkElement).toBeInTheDocument();
+	});
+
+	it('find forgot button in the login page', () => {
+		const wrapper = shallow(
+			<Router>
+				<LoginPage />
+			</Router>
+		);
+		expect(wrapper.find('button.ForgotMsg')).toHaveLength(0);
+	});
+
+	it('find link registration page link', () => {
+		const { getByText } = render(
+			<Router>
+				<LoginPage />
+			</Router>
+		);
+		const linkElement = getByText(/Sign Up Instead/i);
+		expect(linkElement).toBeInTheDocument();
+	});
 });
