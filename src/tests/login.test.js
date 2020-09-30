719b2476193eb362b8fa2b15ab8ee0b7d1f6c626
nishakshatriya <nisha.kshatriya08@gmail.com>
2020-09-30 14:23:32 +0530

    feat: Added testcases for Registration page

--- src/tests/login.test.js
+++ src/tests/login.test.js
@@ -3,6 +3,7 @@ import Enzyme, { shallow } from 'enzyme';
 import { render } from '@testing-library/react';
 import Adapter from 'enzyme-adapter-react-16';
 import LoginPage from '../components/login';
+import RegistrationPage from '../components/register';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 React.useLayoutEffect = React.useEffect;
@@ -47,6 +48,47 @@ describe('Login page components', () => {
 			</Router>
 		);
 		const linkElement = getByText(/Sign Up Instead/i);
+		expect(linkElement).toBeInTheDocument();
+	});
+});
+
+describe('Register page components', () => {
+	it('get text of create Account', () => {
+		const { getByText } = render(
+			<Router>
+				<RegistrationPage />
+			</Router>
+		);
+		const linkElement = getByText('Create your Google Account');
+		expect(linkElement).toBeInTheDocument();
+	});
+
+	it('get text of password helper text', () => {
+		const { getByText } = render(
+			<Router>
+				<RegistrationPage />
+			</Router>
+		);
+		const linkElement = getByText('Use 8 or more with mix of letters, numbers and symbol');
+		expect(linkElement).toBeInTheDocument();
+	});
+
+	it('find forgot button in the login page', () => {
+		const wrapper = shallow(
+			<Router>
+				<RegistrationPage />
+			</Router>
+		);
+		expect(wrapper.find('button.nextButton')).toHaveLength(0);
+	});
+
+	it('find link registration page link', () => {
+		const { getByText } = render(
+			<Router>
+				<RegistrationPage />
+			</Router>
+		);
+		const linkElement = getByText(/Sign In Instead/i);
 		expect(linkElement).toBeInTheDocument();
 	});
 });
